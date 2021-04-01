f9a53182924aaa444b9da09f713ac1e29d617ea5
sooyeon <20200918@sungshin.ac.kr>
2021-04-01 21:03:09 +0900

    Initial Commit

---
+++ 2-1 자료구조 구현/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  2-1 자료구조 구현
+//
+//  Created by 수연 on 2021/04/01.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
