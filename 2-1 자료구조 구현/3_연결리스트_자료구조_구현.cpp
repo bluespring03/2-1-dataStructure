e302a3647546ef56785d00e489317c2339d8e68e
sooyeon <20200918@sungshin.ac.kr>
2021-04-04 02:26:33 +0900

    자료구조 포인터 개념^^><

--- 2-1 자료구조 구현/3_연결리스트_자료구조_구현.cpp
+++ 2-1 자료구조 구현/3_연결리스트_자료구조_구현.cpp
@@ -4,23 +4,72 @@
 //
 //  Created by 수연 on 2021/04/01.
 //
-
 #include <iostream>
 using namespace std;
+#include <stdlib.h>
+
+typedef struct _node{
+    int data;
+    struct _node *next;
+} Node;
 
 int main(){
-    int arr[10];
-    int readCount =0;
-    int readData=0;
-    int i;
+    Node *head = NULL;
+    Node *tail=NULL;
+    Node *cur=NULL; //저장된 데이터의 조회에 사용되는 포인터 변수
     
+    Node *newNode = NULL;
+    int readData;
+    
+    //데이터 입력 과정
     while(1){
-        cout<<"put nummber: ";
-        cin>>readData;
+        cout<<"자연수를 입력하라: "; cin>>readData;
         if(readData<1) break;
         
-        arr[readCount++]=readData;
+        newNode =(Node*)malloc(sizeof(Node));
+        newNode -> data = readData;
+        newNode ->next = NULL;
+        
+        if(head==NULL)
+            head=newNode;
+        else
+            tail->next = newNode;
     }
-    for(i=0; i<readCount; i++) printf("%d ", arr[i]); //cout<<arr[i]<<" "보다 printf%d가 간편함
+    
+    //입력받은 데이터 출력과정
+    cout<<"\n데이터 전체 출력";
+    if(head==NULL) cout<<"nothing\n";
+    else{
+        cur=head;
+        cout<<cur->data;
+        
+        while(cur->next!=NULL){
+            cur=cur->next;
+            cout<<cur->data;
+        }
+    }
+    
+    
+    //메모리 해체과정
+    if(head == NULL) return 0;
+    else{
+        Node*delNode=head;
+        Node*delNextnode=head->next;
+        
+        cout<<"\n"<<head->data<<"를 삭제합니다.\n";
+        free(delNode);
+        
+        while(delNextnode!= NULL){
+            delNode=delNextnode;
+            delNextnode = delNextnode->next;
+            
+            cout<<delNode->data<<"를 삭제합니다.";
+            free(delNode);
+        }
+    }
     return 0;
+    
+    
+    
 }
+
