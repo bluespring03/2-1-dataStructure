7a5254f97c0895f1a677b4b4085d9c85aded5947
sooyeon <20200918@sungshin.ac.kr>
2021-06-14 23:57:58 +0900

    하 미치겠다진짜 넘어렵게잡았나

---
+++ 2-1 자료구조 구현/homework.cpp
@@ -0,0 +1,16 @@
+//
+//  homework.cpp
+//  2-1 자료구조 구현
+//
+//  Created by 수연 on 2021/06/10.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    /**엘레베이터 자료 구현*/
+    cout<<"hello";
+    cout<< "\n homework 파이팅";
+    return 0;
+}
